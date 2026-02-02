670d9c9b31b069f411852d12d2a5b7059ac03f16
CaioDevFlutter <114621145+CaioReis29@users.noreply.github.com>
2026-02-02 20:45:07 -0300

    chore: add profile edit and profile pages

---
+++ lib/presentation/screens/profile/profille_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class ProfillePage extends StatefulWidget {
+
+  const ProfillePage({ super.key });
+
+  @override
+  State<ProfillePage> createState() => _ProfillePageState();
+}
+
+class _ProfillePageState extends State<ProfillePage> {
+
+   @override
+   Widget build(BuildContext context) {
+       return Scaffold(
+           appBar: AppBar(title: const Text(''),),
+           body: Container(),
+       );
+  }
+}
